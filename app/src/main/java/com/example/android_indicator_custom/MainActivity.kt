2751004b98337b2315737754e14f5b533c057646
onyou <onyou0031@naver.com>
2020-12-10 08:34:20 +0900

    리모트 백업 ..

--- app/src/main/java/com/example/android_indicator_custom/MainActivity.kt
+++ app/src/main/java/com/example/android_indicator_custom/MainActivity.kt
@@ -6,8 +6,6 @@ import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
-//    private val viewPager = findViewById<ViewPager>(R.id.viewPager)
-//    private val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,30 +20,8 @@ class MainActivity : AppCompatActivity() {
 
         viewPager.adapter = pagerAdapter
         tabLayout.setupWithViewPager(viewPager)
-        val view1 = layoutInflater.inflate(R.layout.custom_tab, null)
-        val view2 = layoutInflater.inflate(R.layout.custom_tab, null)
-        val view3 = layoutInflater.inflate(R.layout.custom_tab, null)
-        tabLayout.getTabAt(0)?.customView = view1
-        tabLayout.getTabAt(1)?.customView = view2
-        tabLayout.getTabAt(2)?.customView = view3
-//        setupViewPagerWithTabLayout()
-//        setupCustomIndicator()
-    }
-
-    private fun setupViewPagerWithTabLayout() {
-//        val pagerAdapter = ViewPagerAdapter(supportFragmentManager)
-//        pagerAdapter.addFragment(OneFragment())
-//        pagerAdapter.addFragment(TwoFragment())
-//        pagerAdapter.addFragment(ThreeFragment())
-//
-//        viewPager.adapter = pagerAdapter
-//        tabLayout.setupWithViewPager(viewPager)
-    }
-
-    fun setupCustomIndicator() {
-//        val view = layoutInflater.inflate(R.layout.custom_tab, null)
-//        tabLayout.getTabAt(0)?.customView = view
-//        tabLayout.getTabAt(1)?.customView = view
-//        tabLayout.getTabAt(2)?.customView = view
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            tabLayout.clipToOutline = true
+        }
     }
 }
